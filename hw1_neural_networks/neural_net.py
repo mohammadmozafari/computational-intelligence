5a942f0f2c3785ac82bdfb96a77295a00bb3215a
mohammad <mozafari.mmd@gmail.com>
2020-04-15 15:36:44 +0430

    plots prediction of train and test data.

--- hw1_neural_networks/neural_net.py
+++ hw1_neural_networks/neural_net.py
@@ -30,12 +30,12 @@ def sigmoid(x):
     s = 1 / (1 + np.exp(-x))
     return s
 
-def q1_scatter_plot():
+
+def q1_scatter_plot(x_train, y_train, x_test, y_test):
     """
     This function plots the data as a scatter plot using
     matplotlib library.
     """
-    x_train, y_train, x_test, y_test = read_data()
     train_c0 = x_train[y_train == 0, :]
     train_c1 = x_train[y_train == 1, :]
     test_c0 = x_test[y_test == 0, :]
@@ -62,14 +62,19 @@ def q2_compute_gradient(x, y0, W, b):
     A = x @ W + b                                                   # A.shape = (180, )
     y = sigmoid(A)                                                  # y.shape = (180, )
     cost = -1 * np.sum(y0 * np.log(y) + (1 - y0) * np.log(1 - y))   # cost.shape = (180, )
-    dy = -(y0 * (y ** -1) - (1 - y0) * ((1 - y) ** -1))            # dcost/dy
-    dA = dy * (y * (1 - y))                       # dcost/dA
+    dy = -(y0 * (y ** -1) - (1 - y0) * ((1 - y) ** -1))             # dcost/dy
+    dA = dy * (y * (1 - y))                                         # dcost/dA
     dW = x.T @ dA                                                   # dcost/dW
     db = np.sum(dA)                                                 # dcost/db
     return cost, dW, db
 
 
-def train_linear_network(x_train, y_train):
+def q3_train_network(x_train, y_train):
+    """
+    This function initializes W and b using normal distribution
+    and then uses train data to train them.
+    It returns W and b.
+    """
     W = np.random.normal(0, 1, (2, ))
     b = np.random.normal(0, 1, (1, ))
     n_epoch = 1000
@@ -81,16 +86,23 @@ def train_linear_network(x_train, y_train):
         print('epoch {}: cost = {}'.format(i+1, cost))
     return W, b
 
-def q3_predict():
-    x_train, y_train, x_test, y_test = read_data()
-    W, b = train_linear_network(x_train, y_train)
+def q4_predict(W, b):
+    """
+    This function takes the train weights for the linear model
+    and predicts train and test labels. Then it plots the result.
+    """
     y_train_predicted = (x_train @ W + b >= 0.5) * 1
     y_test_predicted = (x_test @ W + b >= 0.5) * 1
-
     train_acc = np.sum(y_train_predicted == y_train) / x_train.shape[0]
     test_acc = np.sum(y_test_predicted == y_test) / x_test.shape[0]
     print('train accuracy =', train_acc)
     print('test accuracy =', test_acc)
 
-q3_predict()
-# q1_scatter_plot()
+    q1_scatter_plot(x_train, y_train_predicted, x_test, y_test_predicted)
+
+if __name__ == '__main__':
+    x_train, y_train, x_test, y_test = read_data()
+    # q1_scatter_plot(x_train, y_train, x_test, y_test)
+    W, b = q3_train_network(x_train, y_train)
+    q4_predict(W, b)
+
