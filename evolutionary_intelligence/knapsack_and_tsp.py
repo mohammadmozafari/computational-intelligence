d385064a2d42260c36b384318889ea9e0bca2c7a
mohammad <mozafari.mmd@gmail.com>
2020-05-18 15:25:57 +0430

    added tsp fitness function.

--- evolutionary_intelligence/knapsack_and_tsp.py
+++ evolutionary_intelligence/knapsack_and_tsp.py
@@ -11,10 +11,23 @@ def init_tsp_pop(size, num_cities):
     """
     initial_population = []
     for i in range(size):
-        order = list(range(num_cities))
+        order = list(range(num_cities - 1))
         rnd.shuffle(order)
         initial_population.append(order)
     return initial_population
+
+def tsp_fit(route, env):
+    """
+    Computes the length of given route.
+    The fitness equals minus length.
+    """
+    length = 0.0
+    curr_x, curr_y = env[-1]
+    for city in route:
+        next_x, next_y = env[city]
+        length += (curr_x - next_x) ** 2 + (curr_y - next_y) ** 2
+        curr_x, curr_y = next_x, next_y
+    return -length
 
 if __name__ == "__main__":
     main()
