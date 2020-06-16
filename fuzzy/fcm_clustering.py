4679dc7bccd7986780b151a5965e03fcc782d251
mohammad <mozafari.mmd@gmail.com>
2020-06-16 10:09:29 +0430

    clusters-cost plot

--- fuzzy/fcm_clustering.py
+++ fuzzy/fcm_clustering.py
@@ -3,13 +3,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def main():
-    data = get_data('sample1.csv')
+    data = get_data('sample4.csv')
     # plot_data(data)
-    # C = find_optimum_c(data)
-    C = 4
-    m = 1.5
-    centroids, cost, pbmf = cluster_data(data, C, m)
-    plot_clusters(data, centroids)
+    try_different_c(data, range(1, 10))
+    # C = 4
+    # m = 1.5
+    # centroids = cluster_data(data, C, m)
+    # plot_clusters(data, centroids)
 
 def get_data(file):
     """
@@ -32,9 +32,6 @@ def plot_data(data):
     plt.ylabel('X 2')
     plt.show()
 
-def find_optimum_c(data):
-    pass
-
 def cluster_data(data, C, m=1.5, eps=1e-5):
     """
     Executes the FCM algorithm until convergence.
@@ -44,19 +41,54 @@ def cluster_data(data, C, m=1.5, eps=1e-5):
     U = (U.T/U.sum(axis=1)).T
     while True:
         um = U ** m
-        c = um.T @ data
-        c = (c.T/um.sum(axis=0)).T
+        centroids = um.T @ data
+        centroids = (centroids.T/um.sum(axis=0)).T
         oldU = U.copy()
-        U = -2 * data @ c.T
-        U += np.sum(c**2, axis=1)
-        U = (U.T + np.sum(data**2, axis=1)).T
-        U = U ** (1/(m-1))
-        U = 1/U
-        U = (U.T/U.sum(axis=1)).T
+        U = calculate_U(data, centroids, m)
         error = np.sum(np.abs(U-oldU))
         print('diff:', error)
         if error < eps:
             break
+    return centroids, U
+
+def calculate_U(data, centroids, m):
+    """
+    Uses the distribution of data and current
+    cluster centroids to determine how much each
+    datum belongs to each cluster.
+    """
+    U = -2 * data @ centroids.T
+    U += np.sum(centroids**2, axis=1)
+    U = (U.T + np.sum(data**2, axis=1)).T
+    U = U ** (1/(m-1))
+    U = 1/U
+    U = (U.T/U.sum(axis=1)).T
+    return U
+
+def try_different_c(data, c_set, m=1.5):
+    """
+    Clusters data with different number of clusters
+    and returns results to choose the best c.
+    """
+    costs = []
+    for c in c_set:
+        centroids, U = cluster_data(data, c, m)
+        cost = calculate_cost(data, centroids, U, m)
+        costs.append(cost)
+    plt.plot(c_set, costs)
+    plt.show()
+
+def calculate_cost(data, centroids, U, m):
+    """
+    Using the distribution of data, cluster centroids
+    and the membership of each datum to each cluster
+    calculates the cost of clustering.
+    """
+    distance = -2 * data @ centroids.T
+    distance += np.sum(centroids**2, axis=1)
+    distance = (distance.T + np.sum(data**2, axis=1)).T
+    cost = np.sum(distance * (U ** m))
+    return cost
 
 def plot_clusters(data, centroids):
     pass
